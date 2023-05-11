717e5814645c54abf9cf8d8e312cca22494d9dac
mcastillomartini <109638654+mcastillomartini@users.noreply.github.com>
2023-05-10 20:04:07 -0600

    Add files via upload
    Uso de funciones

---
+++ parametros.js
@@ -0,0 +1,25 @@
+
+
+function suma(x,y){
+    let val = x+y;
+    return val;
+}
+
+let a = 540;
+let b = 8745;
+
+
+let res = suma(8,100);
+console.log(res);
+
+res = suma(a,b);
+console.log(res);
+
+res = suma(85,15) *3;
+console.log(res);
+
+res = suma();
+console.log(res);
+
+res = suma(0,0);
+console.log(res);
